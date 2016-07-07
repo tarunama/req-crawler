5b44ab46c935455582677253f55e70e68b50007e
meganeski <yamamoto@marici.co.jp>
2016-07-07 13:38:49 +0900

    Add settings.

---
+++ settings/base.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+CRAWLED_URLS = [
+    'https://www.wantedly.com/search?q=python',
+]
---
+++ settings/develop.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import pymysql
+
+
+DB_SETTINGS = {
+    'host': 'localhost',
+    'user': 'req',
+    'password': 'req',
+    'db': 'req',
+    'charset': 'utf8',
+    'cursorclass': pymysql.cursors.DictCursor
+}
+
+DB_TABLES = [
+    'requirement'
+]
+
+DB_ROWS = {
+    'requirement': [
+        'id int',
+        'language varchar(64)',
+        'company_name varchar(255)',
+        'media varchar(255)',
+        'create_at datetime',
+        'update_at datetime',
+    ]
+}
