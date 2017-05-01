8c0f3c20f1438295f8b0d78c1918476725bf6bc5
sakeharami <sakeharami@gmail.com>
2017-05-01 20:47:57 +0900

    Add pattern for creating some file logger.

--- req_crawler/logger/logger.py
+++ req_crawler/logger/logger.py
@@ -3,19 +3,25 @@ import logging
 from subprocess import call
 
 
-def create_file_handler():
+def create_file_handler(log_type):
+    log_name = ('req_crawler.log' if log_type == 'main' else
+                'task.log')
+    file_name = '../log/{}'.format(log_name)
+
     try:
-        handler = logging.FileHandler(filename='../log/req_crawler.log')
+        handler = logging.FileHandler(filename=file_name)
     except FileNotFoundError:
-        call('touch ../log/req_crawler.log')
-        handler = logging.FileHandler(filename='../log/req_crawler.log')
+        touch_command = 'touch {}'.format(file_name)
+        call(touch_command)
+        handler = logging.FileHandler(filename=file_name)
+
     return handler
 
 
 def create_logger():
     logger = logging.getLogger('req_crawler')
     logger.setLevel(logging.INFO)
-    handler = create_file_handler()
+    handler = create_file_handler('main')
     formatter = \
         logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
@@ -28,7 +34,7 @@ logger = create_logger()
 def create_process_logger():
     task_logger = logging.getLogger('req_crawler:process')
     task_logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(filename='../log/task.log')
+    handler = create_file_handler('task')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
     handler.setFormatter(formatter)
     task_logger.addHandler(handler)
