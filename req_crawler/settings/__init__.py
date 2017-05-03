f073326509fc7d8a0373cca42983313e58a6ffc2
sakeharami <sakeharami@gmail.com>
2017-05-04 07:31:26 +0900

    モジュールへのパスを修正

--- req_crawler/settings/__init__.py
+++ req_crawler/settings/__init__.py
@@ -5,5 +5,5 @@ from importlib import import_module
 
 __all__ = ['environ', 'settings']
 
-environ = os.environ.get('REQ_CRAWLER_SETTINGS', 'settings.base')
+environ = os.environ.get('REQ_CRAWLER_SETTINGS', 'req_crawler.settings.base')
 settings = import_module(environ)
